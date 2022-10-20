9bb65e5d885647f9c3d1b4531c4ddccc40b4b795
Nicholas Zehr <nicholas.zehr@icloud.com>
2022-10-20 09:19:10 -0500

    20OCT2022 useOutletContext installed.

--- src/components/PostDetails.jsx
+++ src/components/PostDetails.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import { useOutletContext, Link } from "react-router-dom";
 const PostDetails = (props) => {
-return (
- <div className='box'>{`This is your PostDetails Component`} </div>
-)}
+    const context = useOutletContext()
+    const post = context[0]
+    console.log(post)
+    return (
+        <>
+            {post ? <div className="box">
+      <div>{post.title} </div>
+      <div>{post.description} </div>
+      <div>{post.price} </div>
+      <div>{post.location} </div>
+      <Link to={'/posts'}>
+        <button>Go Back</button>
+      </Link></div> : <div>Loading your post details...</div>
+            }
+    </>
+  );
+};
 
-export default PostDetails
+export default PostDetails;
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -12,9 +12,14 @@ const Posts = (props) => {
         }
         fetchPosts()
     }, [])
+    function filterPosts() {
+        return posts.filter((post) => {
+            return post._id == id
+        })
+    }
 return (
     <div className='posts'>{`This is your Posts Component`} 
-    {id ? <Outlet/> :
+        {id ? <Outlet context={filterPosts()} /> :
             posts.length ? posts.map((post) => {
                 return <SinglePost key={`post-id-${post._id}`} post={post} />
         }) : <div>Loading Your Posts</div>
